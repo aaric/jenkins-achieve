22c575fab4dc436bfebee1eaa4761bc3bfcf3ea4
aaric <vipaaric@gmail.com>
2018-05-08 16:28:47 +0800

    0.0.1-SNAPSHOT init

---
+++ src/test/java/JenkinsTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * JenkinsTest
+ *
+ * @author Aaric, created on 2018-05-08T16:23.
+ * @since 0.0.1-SNAPSHOT
+ */
+public class JenkinsTest {
+
+    @Test
+    public void testPrint() {
+        System.out.println("hello");
+    }
+}
